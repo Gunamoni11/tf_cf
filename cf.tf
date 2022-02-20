cff4d7196c6ef796c40cbc28e83e4cfbee45ba6c
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-02-20 09:03:28 +0530

    Update cf.tf

--- cf.tf
+++ cf.tf
@@ -5,5 +5,5 @@ resource "aws_cloudformation_stack" "my-new-stack" {
     Port="22"
     VpcId = "${var.vpc_id}"
   }
-  template_body = "${file("${path.module}/mystack.yml")}"
+  template_body = "${file("${path.module}/cf.tf")}"
 }
