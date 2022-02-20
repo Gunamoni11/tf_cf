c9a1a400ec24eac5adc96f592f1e79c122d5f11c
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-02-20 09:14:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,4 @@ provider "aws"{
   region = "us-east-2"
 }
 module "cf" {
-  source = "https://github.com/Gunamoni11/tf_cf.git?ref=module"
+  source = "https://github.com/Gunamoni11/tf_cf.git?ref=master"
