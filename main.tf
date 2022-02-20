98f90648a1f8343f3f893ff5d7e4d7f46bd3da3d
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-02-20 09:48:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudformation_stack" "example" {
   name = "example"
   parameters = {
-    VpcId = var.vpc_id
+    VpcId = vpc-6014600b
   }
   template_body = file("${path.master}/example.yml")
 }
